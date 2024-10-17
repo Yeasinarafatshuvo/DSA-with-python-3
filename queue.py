bdd4e019233a10b337160c9fd9f153d278b282a6
Yeasin Arafat <yeasinshuvo76@gmail.com>
2024-10-17 12:33:43 +0600

    Create queue.py

---
+++ queue.py
@@ -0,0 +1,28 @@
+class Queue:
+
+    def __init__(self):
+        self.queue = []
+    
+    def is_empty(self):
+        return len(self.queue) == 0
+    
+    def enqueue(self, item):
+        self.queue.append(item)
+
+    def dequeue(self):
+        if self.is_empty():
+            raise IndexError("Dequeue from an empty queue")
+        return self.queue.pop(0)
+    
+    def peek(self):
+        if self.is_empty():
+            raise IndexError("Peek from an empty queue")
+        return self.queue[0]
+    
+    def size(self):
+        return len(self.queue)
+    
+    def clear(self):
+        self.queue = []
+
+    
